86366616689837ea5fccac4db4637c3d2e19d74e
looklikeapro <jerryzhou00@gmail.com>
2015-03-11 23:46:03 -0400

    connect to bloomberg api

--- main.js
+++ main.js
@@ -1,7 +1,47 @@
 var assert = require("assert"),
-    brain = require("./brain/lib/brain.js");
+    brain = require("brain/lib/brain.js");
 
-var blpapi = require("./blpapi/blpapi.js");
+
+var blpapi = require("blpapi");
+
+//var session = new blpapi.Session({serverHost: hp.serverHost, serverPort: hp.serverPort});
+var session = new blpapi.Session({host: '127.0.0.1', port: 8194});
+
+var service_mktdata = 1; // Unique identifier for mktdata service
+
+var seclist = ['AAPL US Equity', 'VOD LN Equity'];
+
+session.on('SessionStarted', function(m) {
+    console.log(m);
+    console.log("lol");
+    session.openService('//blp/mktdata', service_mktdata);
+});
+
+session.on('ServiceOpened', function(m) {
+    c.log(m);
+    // Check to ensure the opened service is the mktdata service
+    if (m.correlations[0].value == service_mktdata) {
+        // Subscribe to market bars for each security
+        session.subscribe([
+            { security: seclist[0], correlation: 100,
+              fields: ['LAST_PRICE', 'BID', 'ASK'] },
+            { security: seclist[1], correlation: 101,
+              fields: ['LAST_PRICE', 'BID', 'ASK'] }
+        ]);
+    }
+});
+
+session.on('MarketDataEvents', function(m) {
+    c.log(m);
+    // At this point, m.correlations[0].value will equal:
+    // 100 -> MarketBarStart for AAPL US Equity
+    // 101 -> MarketBarStart for VOD LN Equity
+});
+
+session.start();
+
+
+
 
 var net = new brain.NeuralNetwork();
 
@@ -14,16 +54,13 @@ var not = [{input: [1], output: [0]},
            {input: [0], output: [1]}];
 
 var trainStream = net.createTrainStream({
-  /**
-   * Write training data to the stream. Called on each training iteration.
-   */
+  //Write training data to the stream. Called on each training iteration.
+
   floodCallback: function() {
     flood(trainStream, xor);
   },
 
-  /**
-   * Called when the network is done training.
-   */
+  //Called when the network is done training.
   doneTrainingCallback: function(obj) {
     console.log("trained in " + obj.iterations + " iterations with error: "
                 + obj.error);
